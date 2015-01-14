a2346c71932812910398cec97f4d79241487bb82
woldemar <woldemar@mimas.ru>
2015-01-15 00:08:26 +0300

    separate audio streams and reorder flags for readability

--- ffgrab-gl-v4l.sh
+++ ffgrab-gl-v4l.sh
@@ -8,15 +8,20 @@
 source config.sh
 
 glgrab64-av ffmpeg \
--vsync cfr \
--f v4l2 $VINPUT_CAM_PARAMS -i ${VINPUT_CAM_DEV} \
--f pulse $AINPUT_MIC_PARAMS -i ${AINPUT_MIC_DEV} \
--f glgrab $VINPUT_GL_PARAMS -i ${VINPUT_GL_DEV} \
--f pulse $AINPUT_SND_PARAMS -i ${AINPUT_SND_DEV} \
--vpre:0 cinelerra -apre:1 cinelerra -pix_fmt:0 yuv420p \
--f mov -movflags faststart -map 0 -map 1 -r:v 30 \
-${OUTPUT_FILENAME_PREFIX}_cam.mov \
--vpre:2 cinelerra -apre:3 cinelerra -pix_fmt:2 yuv420p \
--f mov -movflags faststart -map 2 -map 3 -r:v 30 \
-${OUTPUT_FILENAME_PREFIX}_scr.mov
-
+	    -vsync cfr \
+	    -f v4l2 $VINPUT_CAM_PARAMS -i ${VINPUT_CAM_DEV} \
+	    -f pulse $AINPUT_MIC_PARAMS -i ${AINPUT_MIC_DEV} \
+	    -f glgrab $VINPUT_GL_PARAMS -i ${VINPUT_GL_DEV} \
+	    -f pulse $AINPUT_SND_PARAMS -i ${AINPUT_SND_DEV} \
+	    -vpre:0 cinelerra -pix_fmt:0 yuv420p \
+	    -f mov -movflags faststart -map 0 -r:v 30 \
+	    ${OUTPUT_FILENAME_PREFIX}_cam.mov \
+	    -apre:1 cinelerra \
+	    -f wav -map 1 \
+	    ${OUTPUT_FILENAME_PREFIX}_mic.wav \
+	    -vpre:2 cinelerra -pix_fmt:2 yuv420p \
+	    -f mov -movflags faststart -map 2 -r:v 30 \
+	    ${OUTPUT_FILENAME_PREFIX}_scr.mov \
+	    -apre:3 cinelerra \
+	    -f wav -map 3 \
+	    ${OUTPUT_FILENAME_PREFIX}_snd.wav
--- ffgrab-x11.sh
+++ ffgrab-x11.sh
@@ -12,6 +12,12 @@ glgrab64-av ffmpeg \
 	    -f x11grab $VINPUT_X11_PARAMS -i ${VINPUT_X11_DEV} \
 	    -f pulse $AINPUT_SND_PARAMS -i ${AINPUT_SND_DEV} \
 	    -f pulse $AINPUT_MIC_PARAMS -i ${AINPUT_MIC_DEV} \
-	    -vpre:0 cinelerra -apre:1 cinelerra -apre:2 cinelerra -pix_fmt:0 yuv420p \
-	    -f mov -movflags faststart -map 0 -map 1 -map 2 -r:v 30 \
-	    ${OUTPUT_FILENAME_PREFIX}_scr.mov
+	    -vpre:0 cinelerra -pix_fmt:0 yuv420p \
+	    -f mov -movflags faststart -map 0 -r:v 30 \
+	    ${OUTPUT_FILENAME_PREFIX}_scr.mov \
+	    -apre:1 cinelerra \
+	    -f wav -map 1 \
+	    ${OUTPUT_FILENAME_PREFIX}_snd.wav \
+	    -apre:2 cinelerra \
+	    -f wav -map 2 \
+	    ${OUTPUT_FILENAME_PREFIX}_mic.wav
